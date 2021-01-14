b1f78dfd15a43448fd80e60e9c63a95fcba12e8d
SoYeong Choi <thdud3410@gmail.com>
2021-01-14 22:36:09 +0900

    Create 선자르기_bottom_up.py

---
+++ DP/선자르기_bottom_up.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+#sys.stdin=open("input.txt","rt")
+
+n=int(input())
+dy=[0]*(n+1)
+dy[1]=1
+dy[2]=2
+
+for i in range(3,n+1):
+    dy[i]=dy[i-1]+dy[i-2]
+
+print(dy[n])
