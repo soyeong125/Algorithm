027ac1fa7c43057849b912e5db8bf5d639f6f14f
SoYeong Choi <thdud3410@gmail.com>
2021-07-26 23:56:56 +0900

    1863

---
+++ BaekJoon/16953.py
@@ -0,0 +1,20 @@
+
+import sys
+sys.stdin=open("input.txt","rt")
+
+def DFS(L,val,goal):
+    if val == goal:
+        print(L+1)
+        sys.exit()
+    if val > goal:
+        return
+    DFS(L+1,val*2,goal)
+    DFS(L+1,val*10+1,goal)
+
+
+if __name__ == "__main__":
+    a,b = map(int,input().split())
+    DFS(0,a,b)
+    print(-1)
+
+
