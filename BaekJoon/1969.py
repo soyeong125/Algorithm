80041ed5d3cb344cc287c58513f801987a3cbd05
SoYeong Choi <thdud3410@gmail.com>
2021-08-04 00:16:28 +0900

    그리디

--- BaekJoon/1969.py
+++ BaekJoon/1969.py
@@ -3,20 +3,18 @@ sys.stdin=open("input.txt","rt")
 
 n,m = map(int,input().split())
 arr=sorted([input() for _ in range(n)])
-result=""
-minn=2176000000
-for i in range(n):
-    cnt=0
+result=[]
+cnt=0
+for i in range(m):
+    rna={'T':0,'A':0,'G':0,'C':0}
     for j in range(n):
-        if i==j:
-            continue
-        for k in range(m):
-            if arr[i][k] != arr[j][k]:
-                cnt+=1
-    if cnt < minn:
-        minn = cnt
-        result = arr[i]
-print(result)
-print(minn)
+        rna[arr[j][i]]+=1
+    tmp = sorted(rna.items(),key=lambda x:(-x[1],x[0]))
+    result.append(tmp[0][0])
+    cnt+=(n-tmp[0][1])
+print("".join(result))
+print(cnt)
+
+
 
 
