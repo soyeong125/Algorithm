079b0221cc329cd937f32151fd4e9400c886a3a6
SoYeong Choi <thdud3410@gmail.com>
2022-08-09 00:36:16 +0900

    시간초과 해결 > for문 제거

--- BaekJoon/2661.py
+++ BaekJoon/2661.py
@@ -1,34 +1,23 @@
 import sys
-import math
+
 sys.stdin = open("input.txt", 'r')
 
 
-def dfs(result,op_list):
-    global visited
-    global n
+def dfs(depth,total,pluse,minus,multiply,divide):
     global max_answer
     global min_answer
-    global num
-    if sum(op_list) == 0:
-        max_answer = max(max_answer,result)
-        min_answer = min(min_answer,result)
+    if depth == n:
+        max_answer = max(max_answer,total)
+        min_answer = min(min_answer,total)
         return
-    for i in range(1,n):
-        if not visited[i]:
-            visited[i]=1
-            for j in range(4):
-                if op_list[j] > 0:
-                    tmp_list = op_list[:]
-                    tmp_list[j]-=1
-                    if j==0:
-                        dfs(result+num[i],tmp_list)                            
-                    elif j==1:
-                        dfs(result-num[i],tmp_list)                                               
-                    elif j==2:  
-                        dfs(result*num[i] ,tmp_list)                                                 
-                    else:
-                        dfs(int(result/num[i]),tmp_list)                                                          
-            visited[i]=0    
+    if pluse:
+        dfs(depth+1,total+num[depth],pluse-1,minus,multiply,divide)
+    if minus:
+        dfs(depth+1,total-num[depth],pluse,minus-1,multiply,divide)
+    if multiply:
+        dfs(depth+1,total*num[depth],pluse,minus,multiply-1,divide)
+    if divide:
+        dfs(depth+1,int(total/num[depth]),pluse,minus,multiply,divide-1)       
 
 
 if __name__ == "__main__":
@@ -36,11 +25,10 @@ if __name__ == "__main__":
     num = list(map(int,input().split()))  
     op_list = list(map(int,input().split())) 
     visited = [0 for i in range(n)]
-    max_answer = -1000000
-    min_answer = 1000000
+    max_answer = -1e9
+    min_answer = 1e9
 
-    visited[0]=1
-    dfs(num[0],op_list)
+    dfs(1,num[0],op_list[0],op_list[1],op_list[2],op_list[3])
 
     print(max_answer)
     print(min_answer)
