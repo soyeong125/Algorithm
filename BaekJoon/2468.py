cd77504832869f505d92dc09b15e0e22921fda6c
SoYeong Choi <thdud3410@gmail.com>
2022-09-05 01:03:51 +0900

    dfs

--- BaekJoon/2468.py
+++ BaekJoon/2468.py
@@ -3,39 +3,35 @@ from collections import deque
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+
 if __name__ == "__main__":
     n = int(input())
     area = [list(map(int,input().split())) for _ in range(n)]
-    maxvalue = 0
-    minvalue = 1000000
-    result = 0
-    for i in range(n):
-        for j in range(n):
-            maxvalue = max(maxvalue,area[i][j])
-            minvalue = min(minvalue,area[i][j])
+    result = 1
 
-    for r in range(minvalue,maxvalue+1):
+    def dfs(x,y,r):
+        dx = [1,-1,0,0]
+        dy = [0,0,1,-1]
+        for k in range(4):
+            xx = dx[k] + x
+            yy = dy[k] + y
+            if 0<=xx<n and 0<=yy<n and area[xx][yy] > r and not visited[xx][yy]:
+                visited[xx][yy] = 1
+                dfs(xx,yy,r)
+
+
+
+    for r in range(max(max(area))):
         visited = [[0]*n for _ in range(n)]
         count = 0
-        q = deque()
         for i in range(n):
             for j in range(n):
                 if area[i][j] > r and not visited[i][j]:
-                    q.append([i,j])
                     visited[i][j] = 1
                     count +=1
-                    while q:
-                        x,y = q.popleft()
-                        for k in range(4):
-                            xx = x + dx[k]
-                            yy = y + dy[k]
-                            if 0<=xx<n and 0<=yy<n and not visited[xx][yy]:
-                                if area[xx][yy] > r :
-                                    q.append([xx,yy])
-                                    visited[xx][yy] = 1
+                    dfs(i,j,r)
         result = max(result,count)
+
     print(result)
 
                 
