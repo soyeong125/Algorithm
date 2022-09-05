3808c3de5cfee8ffb99d3855eac909efc3474a31
SoYeong Choi <thdud3410@gmail.com>
2022-09-05 23:05:16 +0900

    DFS 풀이

--- BaekJoon/10026.py
+++ BaekJoon/10026.py
@@ -1,60 +1,64 @@
 import sys
-from collections import deque
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
-#BFS 풀이
+sys.setrecursionlimit(100000)
+
 if __name__ == "__main__":
     n = int(input())
     area = [list(map(str,input())) for _ in range(n)]
-    q = deque()
-    visited = [[0] * n for _ in range(n)]
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
+  
+
+
+    def DFS1(x,y,cur):
+        dx = [1,-1,0,0]
+        dy = [0,0,1,-1]
+        for k in range(4):
+            xx = x + dx[k]
+            yy = y + dy[k]
+            if 0<= xx < n and 0<= yy < n and not visited[xx][yy]:
+                if area[xx][yy] == cur:
+                    visited[xx][yy] = 1
+                    DFS1(xx,yy,cur)
+    def DFS2(x,y,cur):
+        dx = [1,-1,0,0]
+        dy = [0,0,1,-1]
+        for k in range(4):
+            xx = x + dx[k]
+            yy = y + dy[k]
+            if 0<= xx < n and 0<= yy < n and not visited[xx][yy]:
+                if cur == 'R' or cur == 'G':
+                    if area[xx][yy] == 'R' or area[xx][yy] == 'G':
+                        visited[xx][yy]=1
+                        DFS2(xx,yy,cur)
+                else:
+                    if area[xx][yy] == cur:
+                        visited[xx][yy]=1
+                        DFS2(xx,yy,cur)
+
+            
+
 
     cnt1 = 0
+    visited = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
                 visited[i][j] = 1
-                q.append([i,j])
-                cnt1 += 1
-                while q:
-                    x,y = q.popleft()
-                    cur = area[x][y]
-                    for k in range(4):
-                        xx = x + dx[k]
-                        yy = y + dy[k]
-                        if 0 <= xx < n and 0 <= yy < n and not visited[xx][yy]:
-                            if area[xx][yy] == cur:
-                                q.append([xx,yy])
-                                visited[xx][yy]=1
+                cnt1 +=1
+                DFS1(i,j,area[i][j])
+
     cnt2 = 0
     visited = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
                 visited[i][j] = 1
-                q.append([i,j])
-                cnt2 += 1
-                while q:
-                    x,y = q.popleft()
-                    cur = area[x][y]
-                    for k in range(4):
-                        xx = x + dx[k]
-                        yy = y + dy[k]
-                        if 0 <= xx < n and 0 <= yy < n and not visited[xx][yy]:
-                            if cur == 'R' or cur == 'G':
-                                if area[xx][yy] == 'R' or area[xx][yy] == 'G':
-                                    q.append([xx,yy])
-                                    visited[xx][yy]=1 
-                            else:
-                                if area[xx][yy] == cur:
-                                    q.append([xx,yy])
-                                    visited[xx][yy]=1
-    print(cnt1,cnt2)
+                cnt2 +=1
+                DFS2(i,j,area[i][j])
 
-    
-
+    print(cnt1,cnt2)             
+                
+ 
 
 
 
