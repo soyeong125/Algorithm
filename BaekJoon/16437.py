e2cf158e5172c129e79465c3c74748ea3c8812a0
SoYeong Choi <thdud3410@gmail.com>
2022-09-07 23:21:30 +0900

    최종 dfs

--- BaekJoon/16437.py
+++ BaekJoon/16437.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 sys.setrecursionlimit(10**8)
@@ -7,26 +6,25 @@ sys.setrecursionlimit(10**8)
 if __name__ == "__main__":
     n = int(input())
     tree = [[] for _ in range(n+1)]
-    node = [[],[0,0]]
+    node = [0,0]
 
     for i in range(2,n+1):
         t,a,p = input().split()
         a = int(a)
         p = int(p)
         tree[p].append(i)
-        node.append([t,a])
-    
+        if t=="W":
+            node.append(-a)
+        else:
+            node.append(a)
+
     def dfs(v):
-        result = 0
-        
+        result = 0       
         for i in tree[v]:
             result += dfs(i)
-        if node[v][0] == 'W':
-            result -= node[v][1]
-            if result < 0:
-                result = 0
-        else:
-            result += node[v][1]
+        result += node[v]
+        if result < 0:
+            result = 0
         return result
     print(dfs(1))
    
