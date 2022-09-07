3096fb7442cf6a8fce0e96c43ce2ad58138af3ef
SoYeong Choi <thdud3410@gmail.com>
2022-09-07 23:54:49 +0900

    bfs 추가

--- BaekJoon/1987.py
+++ BaekJoon/1987.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
-
+#dfs
 if __name__ == "__main__":
     R,C = map(int,input().split())
     area = [list(map(str,input())) for _ in range(R)]
@@ -25,8 +25,39 @@ if __name__ == "__main__":
     arr[ord(area[0][0])-65] = 1
     DFS(1,0,0)
     print(result)
-    
 
+#bfs    
+import sys
+from collections import deque
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+
+if __name__ == "__main__":
+    r,c = map(int,input().split())
+    area = [list(input()) for _ in range(r)]
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    result = 0
+
+    def bfs():
+        global result
+        q = set([(0,0,area[0][0])])
+        while q:
+            x,y,z = q.pop()
+            result = max(result,len(z))
+            for k in range(4):
+                xx = x + dx[k]
+                yy = y + dy[k]
+                if 0<=xx<r and 0<=yy<c and area[xx][yy] not in z:
+                    q.add((xx,yy,z+area[xx][yy]))
+                    
+   
+    bfs()
+    print(result)
+
+
+  
 
 
 
