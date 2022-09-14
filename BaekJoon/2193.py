fc87584e016a9366037f11ac7c6266d9cf17132a
SoYeong Choi <thdud3410@gmail.com>
2022-09-15 00:50:45 +0900

    success

--- BaekJoon/2193.py
+++ BaekJoon/2193.py
@@ -1,8 +1,13 @@
 import sys
 sys.stdin = open("input.txt", 'r')
-from collections import deque
-import copy
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
 
 if __name__ == "__main__":  
+    n = int(input())
+    dp = [0 for _ in range(n+1)]
+    dp[0]=1
+    dp[1]=1
+
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[n])
