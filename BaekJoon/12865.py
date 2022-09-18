c0c6a216ae9729286456894fcd202a1ca1ab3766
SoYeong Choi <thdud3410@gmail.com>
2022-09-18 22:52:41 +0900

    재귀구현

--- BaekJoon/12865.py
+++ BaekJoon/12865.py
@@ -2,29 +2,23 @@ import sys
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
+
 if __name__ == "__main__":  
     n,k = map(int,input().split())
     arr = [list(map(int,input().split())) for _ in range(n)]
-    arr.sort(key=lambda x:(x[0],x[1]))
-    minweight = arr[0][0]
-    dp = [0] * 1000000
+    
+    def go(i,w):
+        if i == n:
+            return 0
+        n1 = 0
+        n2 = 0
+        if arr[i][0] + w <= k:
+            n1 = go(i+1,w+arr[i][0]) + arr[i][1]
+            n2 = go(i+1,w)
+        return max(n1,n2)
 
-    for weight in range(minweight,k+1):
-        curval = 0
-        curweight = 0
-        for w,v in arr:
-            if w > weight:
-                break
-            if curweight + w > k:
-                curval = max(curval,v)
-            if w < weight and curweight + w <= k:
-                curval += v
-                curweight += w
-        
-        for i in range(minweight,weight//2+1):
-            curval = max(curval,dp[i] + dp[weight - i])
-        dp[weight] = curval
-    print(dp[k])
+    print(go(0,0))
+
 
     
 
