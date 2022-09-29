f47512054701724f3f4cf9730b0cc5b2da96682d
SoYeong Choi <thdud3410@gmail.com>
2022-09-30 00:36:13 +0900

    섬to섬 갈 수 있는지 체크 bfs 이용

--- BaekJoon/1939.py
+++ BaekJoon/1939.py
@@ -1,31 +1,39 @@
 import sys
 sys.stdin = open("input.txt", 'r')
+from collections import deque
 input = sys.stdin.readline
 
 
 if __name__ == "__main__": 
     n,m = map(int,input().split())
     world = [[0] * n for _ in range(n)]
+    adjust_list = [[] for _ in range(n+1)]
     for _ in range(m):
         s,e,v = map(int,input().split())
-        if s < e:
-            world[s-1][e-1] = v    
-        else:
-            world[e-1][s-1] = v
+        adjust_list[s].append([e,v])
+        adjust_list[e].append([s,v])
     v1,v2 = map(int,input().split())
     result = 0
 
-    def dfs(idx,val):
-        global result
-        if not idx == v1 - 1 and result > val:
-            return
-        if idx == v2-1:
-            result = max(result,val)
-            return
-        
-        for i in range(n):
-            if world[idx][i] > 0:
-                dfs(i,min(world[idx][i],val))
-    dfs(v1-1,10000000000)
-    print(result)
-        
+    def bfs():
+        q = deque()
+        q.append(v1)
+        visited = [False] * (n+1)
+        visited[v1] = True
+
+        while q:
+            cur = q.popleft()
+            for e,w in adjust_list[cur]:
+                if not visited[e]:
+                    visited[e] = True
+                    q.append(e)
+                    print(e)
+        return True if visited[v2] else False
+
+    if bfs():
+        print("갈 수 있습니다.")
+    else:
+        print("갈 수 없습니다.") 
+
+
+
