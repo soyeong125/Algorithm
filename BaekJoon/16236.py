89713e678270b81e5ac008a1e087047063d42f9c
SoYeong Choi <thdud3410@gmail.com>
2022-09-30 23:00:06 +0900

    dijkstra 개념 익히기 아자아잦!

--- BaekJoon/16236.py
+++ BaekJoon/16236.py
@@ -1,47 +1,44 @@
+from pickle import FALSE
 import sys
 from collections import deque
-from xml.dom.minidom import Childless
 sys.stdin = open("input.txt", 'r')
-
+input = sys.stdin.readline
 
 if __name__ == "__main__":
-    n,m = map(int,sys.stdin.readline().split())
-    cheese = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-    cnt = 0
+    n = int(input())
+    sea = [list(map(int,input().split())) for _ in range(n)]
+    shark = 2
     q = deque()
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    visited = [[0]*m for _ in range(n)]
     
     for i in range(n):
-        for j in range(m):
-            if cheese[i][j] == 1:
+        for j in range(n):
+            if sea[i][j] == 9:
                 q.append([i,j])
-                tmp_list = []
-                visited[i][j] = 1
-                while q:
-                    x,y = q.popleft()            
-                    air_cnt = 0
-                    for k in range(4):
-                        xx = x + dx[k]
-                        yy = y + dy[k]
-                        if 0<= xx < n and 0 <= yy < m and not visited[xx][yy]:
-                            if cheese[xx][yy] == 0 :
-                                air_cnt +=1
-                            elif cheese[xx][yy] == 1:
-                                q.append([xx,yy])
-                                visited[xx][yy] = 1
-                    if air_cnt >= 2:
-                        cheese[x][y] = -1
-                        tmp_list.append([x,y])
-                visited = [[0]*m for _ in range(n)]     
-                while tmp_list:
-                    s,w = tmp_list.pop()
-                    cheese[s][w]=0
-                cnt +=1
-    print(cnt)
-                        
+                break
+    
+    dx = [1,0,-1,0]
+    dy = [0,-1,0,1]
+    flag = False
+    while True:
+        visited = [[False] * n for _ in range(n)]
+        while q:
+            x,y = q.popleft()
+            for k in range(4):
+                xx = x + dx[k]
+                yy = y + dy[k]
+                if 0<=xx<n and 0<=yy<n and visited[xx][yy] == False:
+                    if 0 < sea[xx][yy] < shark:
+                        q.append([xx,yy])
+                        visited[xx][yy] = True
+                        shark +=1
+                    elif sea[xx][yy] == shark or sea[xx][yy]==0:
+                        q.append([xx,yy])
+                        visited[xx][yy] = True
+        if flag == False:
+            break
+                    
+    print(shark)
 
 
- 
 
+   
--- BaekJoon/1939.py
+++ BaekJoon/1939.py
@@ -1,60 +1,26 @@
 import sys
-sys.stdin = open("input.txt", 'r')
-from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
 
 if __name__ == "__main__": 
     n,m = map(int,input().split())
-    adjust_list = [[] for _ in range(n+1)]
+    world = [[0] * n for _ in range(n)]
     for _ in range(m):
         s,e,v = map(int,input().split())
-        adjust_list[s].append([e,v])
-        adjust_list[e].append([s,v])
-    v1,v2 = map(int,input().split())
-
-    def bfs(c):
-        q = deque()
-        q.append(v1)
-        visited = [False] * (n+1)
-        visited[v1] = True
-
-        while q:
-            cur = q.popleft()
-            if cur == v2:
-                return True
-            for e,w in adjust_list[cur]:
-                if not visited[e] and w >= c:
-                    visited[e] = True
-                    q.append(e)
-
-        return False
-
-    def dfs(start,c):
-        if start == v2:
-            return True
-        for y,w in adjust_list[start]:
-            if not visited[y] and w >= c:
-                visited[y] = True
-                if dfs(y,c):
-                    return True 
-        return False
-
-    
-    l,r = 1,1000000000 #l값을 0으로 해서 탐색하면 메모리 초과
-    ans = 0
-    while l <= r:
-        visited = [False] * (n+1)
-        visited[v1] = True
-        mid = (l+r)//2
-        if dfs(v1,mid):
-            l = mid + 1
-            ans = mid
+        if s < e:
+            world[s-1][e-1] = v    
         else:
-            r = mid -1
-    print(ans)
+            world[e-1][s-1] = v
+    v1,v2 = map(int,input().split())
+    result = 0
 
-
-
-
-
+    def dfs(idx,val):
+        global result
+        if idx == v2-1:
+            result = max(result,val)
+            return
+        
+        for i in range(n):
+            if world[idx][i] > 0:
+                dfs(i,min(world[idx][i],val))
+    dfs(v1-1,10000000000)
+    print(result)
