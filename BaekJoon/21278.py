300b5a70adcb3121716f065af51d934bebbe2e43
SoYeong Choi <thdud3410@gmail.com>
2022-10-29 14:52:53 +0900

    bfs 완탐

--- BaekJoon/21278.py
+++ BaekJoon/21278.py
@@ -5,6 +5,11 @@ sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
 if __name__ == "__main__": 
+    def combinations(arr,r):
+        for i in range(len(arr)):
+            if r == 1:
+                yield [arr[i]]
+            
     n,m = map(int,input().split())
     tree = [[] for _ in range(n+1)]
     for _ in range(m):
