898894230eaa2b80783cab81ca8cb0a498e8d4fa
SoYeong Choi <thdud3410@gmail.com>
2022-11-05 23:33:40 +0900

    bfs + memoization

--- BaekJoon/13913.py
+++ BaekJoon/13913.py
@@ -1,60 +1,30 @@
-# from collections import deque
-# if __name__ == "__main__":  
-#     n,k = map(int,input().split())
-#     q = deque()
-#     visited = [-1 for _ in range(100001)]  
-#     visited[n] = 1
-#     q.append([n,0,str(n)])
-#     while q:
-#         cur,cnt,res = q.popleft()
-#         if cur == k:
-#             print(cnt)
-#             print(res)
-#             break
-#         for i in [cur-1,cur+1,cur*2]:
-#             if 0 <= i < 100001 and visited[i] == -1:
-#                 if i == cur * 2:
-#                     visited[i] = 1
-#                     q.append([i,cnt+1,res+' '+str(i)])
-                        
-#                 else:
-#                     visited[i] = 1
-#                     q.append([i,cnt+1,res+' '+str(i)])
-
 import sys
 from collections import deque
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
-from collections import deque
-if __name__ == "__main__":  
-    n,k = map(int,input().split())
-    dist = [0] * 100001
-    move = [0] * 100001
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    idx = [0] * 100001
+    q = deque()
+    q.append([n,0])
 
-    def printpath(cur):
-        arr = []
-        tmp = cur
-        for _ in range(dist[cur]+1):
-            arr.append(tmp)
-            tmp = move[tmp]
-        print(' '.join(map(str,arr[::-1])))
+    def check(k,cnt):
+        res = [k]
+        i = k
+        for _ in range(cnt):
+            res .append(idx[i])
+            i = idx[i]
+        print(' '.join(map(str,res[::-1])))
 
 
-
-    def bfs():
-        q = deque()
-        q.append(n)
-
-        while q:
-            x = q.popleft()
-            if x == k:
-                print(dist[k])
-                printpath(x)
-                break
-            for i in [x+1,x-1,x*2]:
-                if 0<=i<100001 and dist[i] == 0:
-                    q.append(i)
-                    dist[i] = dist[x] + 1
-                    move[i] = x
-    bfs()
-
+    while q:
+        x,cnt = q.popleft()
+        if x == k:
+            print(cnt)
+            check(x,cnt)
+            break
+        for i in [x-1,x+1,x*2]:
+            if 0 <= i < 100001:
+                if idx[i] == 0:
+                    q.append([i,cnt+1])
+                    idx[i] = x
