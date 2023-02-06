4cbb7c057b7e95b6cb8415e0774bb95807066ef7
SoYeong Choi <thdud3410@gmail.com>
2023-02-06 20:43:00 +0900

    완전탐색/bruteforce/dfs

--- BaekJoon/14888.py
+++ BaekJoon/14888.py
@@ -1,12 +1,49 @@
-
 import sys
-import math
-from itertools import combinations, permutations
 sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
 
 if __name__ == "__main__":
-    n = [1,2,3]
-    print(n[-1:])
-    print(n[-2:-1])
+    n = int(input())
+    number = list(map(int,input().split()))
+    opt = list(map(int,input().split()))
+    maxnum = -1e9
+    minnum = 1e9
+
+    def dfs(l,sum):
+        global opt
+        global maxnum
+        global minnum
+        if l == n :
+            maxnum = max(sum,maxnum)
+            minnum = min(sum,minnum)
+            return
+        
+        if opt[0] > 0 :
+            tmp = sum
+            tmp += number[l]
+            opt[0]-=1
+            dfs(l+1,tmp)
+            opt[0] +=1
+        if opt[1] > 0 :
+            tmp = sum
+            tmp -= number[l]
+            opt[1]-=1
+            dfs(l+1,tmp)
+            opt[1] +=1
+        if opt[2] > 0 :
+            tmp = sum
+            tmp *= number[l]
+            opt[2]-=1
+            dfs(l+1,tmp) 
+            opt[2] +=1
+        if opt[3] > 0 :
+            tmp = sum
+            tmp = int(tmp/number[l])
+            opt[3] -=1
+            dfs(l+1,tmp)
+            opt[3] +=1
+    
+    dfs(1,number[0])    
+    print(maxnum)                 
+    print(minnum)    
