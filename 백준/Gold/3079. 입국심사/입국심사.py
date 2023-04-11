e6e91bbea49fb90bd0b2368a44c0a221e642e504
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-04-11 09:16:17 +0900

    [Gold V] Title: 입국심사, Time: 944 ms, Memory: 35500 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+if __name__ == "__main__":
+    n,m = map(int,input().split())
+    time = [int(input()) for _ in range(n)]
+    time.sort()
+    
+    l = min(time)
+    r = max(time) * m 
+
+    res = r
+    
+
+    while l <= r:
+        target = 0
+        mid = (l + r) // 2 
+
+        for t in time:
+            target +=  mid // t
+            if target >= m :
+                break
+
+        if target >= m :
+            res = min (res,mid)
+            r = mid -1
+        else :
+            l = mid + 1
+            
+    print(res)
