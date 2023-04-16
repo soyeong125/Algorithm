45f4cde48462bcc1205707974d529a0f6b9e7c32
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-04-16 14:08:06 +0900

    [Gold V] Title: 피자 굽기, Time: 324 ms, Memory: 77836 KB -BaekjoonHub

--- 백준/Gold/1756. 피자 굽기/피자 굽기.py
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.py
@@ -1,22 +1,18 @@
-import sys
-input = sys.stdin.readline
-
-
-if __name__ == "__main__":  
-    D,N = map(int,input().split())
+if __name__ == "__main__":
+    d,n = map(int,input().split())
     oven = list(map(int,input().split()))
-    banjuk = list(map(int,input().split()))
+    banjuck = list(map(int,input().split()))
 
-    for i in range(1,D):
-        oven[i] = min(oven[i],oven[i-1])
+    for i in range(1,d):
+        oven[i] = min(oven[i-1],oven[i])
     
     cur = 0
     idx = 0
-    for i in range(D-1,0,-1):
-        if oven[i] < banjuk[cur]:
+    for i in range(d-1,0,-1):
+        if oven[i] < banjuck[cur]:
             continue
         cur +=1
-        if cur >= N:
-            idx = (i+1)
+        if cur >= n :
+            idx = i+1
             break
     print(idx)
