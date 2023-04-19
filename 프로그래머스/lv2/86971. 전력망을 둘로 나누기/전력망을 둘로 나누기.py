edebb174264f5855bd0b5bbe15861ebd8af3692b
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-04-19 13:10:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -13,27 +13,30 @@ def solution(n, wires):
             node2 = wires[j][1]
             tree[node1].append(node2)
             tree[node2].append(node1)
-        visited = [False for _ in range(n+1)]
-        res = [0 for _ in range(n+1)]
+
+        visited = [False for _ in range(n+1)] # 방문한 노드 체크 할 리스트
+        res = [0 for _ in range(n+1)] # 방문한 노드 root 노드 체크
+
         q = deque()
-        q.append([wires[i][0],1]) #자른 간선을 기준으로 체크
-        q.append([wires[i][1],2]) #자른 간선을 기준으로 체크
-        visited[wires[i][0]] = True # 시작 노드 방문 체크
-        visited[wires[i][1]] = True # 시작 노드 방문 체크
-        res[wires[i][0]] = 1
-        res[wires[i][1]] = 2
+        node1 = wires[i][0]
+        node2 = wires[i][1]
+        q.append(node1) #자른 간선을 기준으로 체크
+        q.append(node2) #자른 간선을 기준으로 체크
+        visited[node1] = True # 시작 노드 방문 체크
+        visited[node2] = True # 시작 노드 방문 체크
+        res[node1] = node1
+        res[node2] = node2
 
         while q:
-            node,idx = q.popleft()
-
-            for next_node in tree[node]:
+            cur_node = q.popleft()
+            for next_node in tree[cur_node]:
                 if not visited[next_node]:
                     visited[next_node] = True
-                    res[next_node] = idx
-                    q.append([next_node,idx])
-        ans1,ans2 = 0 , 0
-        ans1 = res.count(1)
-        ans2 = res.count(2)
+                    res[next_node] = res[cur_node]
+                    q.append(next_node)
+                    
+        ans1 = res.count(node1)
+        ans2 = res.count(node2)
         answer = min(answer,abs(ans1-ans2))
 
     return answer
