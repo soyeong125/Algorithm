a52ceb574db23993587e3a23b18787af72896430
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-04-20 11:26:31 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,10 +1,8 @@
 def solution(brown, yellow):
     for i in range(1,yellow+1):
-        a,b = 0,0
-        if yellow % i == 0:
-            a = i
+        a,b = 0,0 # 가로, 세로
+        if yellow % i == 0: # yello 의 약수 구하기
+            a = i 
             b = yellow // i
-        if a > b:
-            break
-        if 2*(a+b) + 4 == brown:
-            return [b+2,a+2]
+        if 2 * (a+b) + 4 == brown: # yellow의 둘레에 4를 더하면 brown 이 된다.
+            return [b+2,a+2] # brown의 가로값, 세로값
