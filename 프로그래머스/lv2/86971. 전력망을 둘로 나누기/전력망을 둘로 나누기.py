5e38fae929b9062ae19abcfb1cf3bdbad852e067
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-04-23 19:58:32 +0900

    Update 전력망을 둘로 나누기.py

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,26 +1,41 @@
-def dfs(node, visited, tree, child):
-    visited[node] = True
-    
-    for next in tree[node]:
-        if not visited[next]:
-            child[node] += dfs(next, visited, tree, child) + child[next]
-        
-    return 1
+import math
 
 def solution(n, wires):
-    tree = [[] for _ in range(n+1)]
-    
-    for v1, v2 in wires:
-        tree[v1].append(v2)
-        tree[v2].append(v1)
-        
-    visited = [False] * (n+1)
-    child = [0] * (n+1) #각자의 자식 개수를 구하기 위한 리스트
-    
-    dfs(1, visited, tree, child)
-    result = n
-    
-    for c in child:
-        result = min(result, abs(n-2*(c+1)))
-    
-    return result
+    answer = 101
+    wires.sort()
+    for i in range(n-1):
+        tree = [[] for _ in range(n + 1)]
+        for j in range(n-1): # 트리 만들기
+            if i == j : # 간선 하나 자르기
+                continue
+            node1 = wires[j][0]
+            node2 = wires[j][1]
+            tree[node1].append(node2)
+            tree[node2].append(node1)
+
+        visited = [False for _ in range(n+1)] # 방문한 노드 체크 할 리스트
+        res = [0 for _ in range(n+1)] # 방문한 노드 root 노드 체크
+
+        q = deque()
+        node1 = wires[i][0]
+        node2 = wires[i][1]
+        q.append(node1) #자른 간선을 기준으로 체크
+        q.append(node2) #자른 간선을 기준으로 체크
+        visited[node1] = True # 시작 노드 방문 체크
+        visited[node2] = True # 시작 노드 방문 체크
+        res[node1] = node1
+        res[node2] = node2
+
+        while q:
+            cur_node = q.popleft()
+            for next_node in tree[cur_node]:
+                if not visited[next_node]:
+                    visited[next_node] = True
+                    res[next_node] = res[cur_node]
+                    q.append(next_node)
+                    
+        ans1 = res.count(node1)
+        ans2 = res.count(node2)
+        answer = min(answer,abs(ans1-ans2))
+
+    return answer
