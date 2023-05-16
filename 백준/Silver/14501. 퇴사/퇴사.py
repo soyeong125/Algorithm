b32a1517d77fabf7edf5c6b230558db07ff9ba59
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-05-16 11:07:05 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,28 +3,13 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    schedule = []
-    visited = [False for _ in range(n)]
+    arr = [list(map(int,input().split())) for _ in range(n)] #상담에 걸리는 시간, 상담 비용
+    dp = [0] * (n+1)
 
-    for _ in range(n):
-        time, pay = map(int, input().split())
-        schedule.append([time, pay])
-    res = 0
+    for day in range(n-1,-1,-1):
+        if day + arr[day][0] > n : #현재 날짜와 상담 날짜를 더해서 퇴사날이면 못함
+            dp[day] = dp[day+1]
+        else:
+            dp[day] = max(dp[day+1],dp[day+arr[day][0]] + arr[day][1]) #상담을 했을 때 비용과 상담 안했을 때 지금까지 누적값중 더 큰 값으로 교체
 
-    def dfs(today, total,lastdaypay):
-        global res
-        if today > n:
-            res = max(res,total-lastdaypay)
-            return
-        elif today == n:
-            res = max(res,total)
-            return
-
-        for i in range(today,n):
-            if not visited[i]:
-                visited[i] = True
-                dfs(i + schedule[i][0], total + schedule[i][1],schedule[i][1])
-                visited[i] = False
-
-    dfs(0,0,0)
-    print(res)
+    print(dp[0])
