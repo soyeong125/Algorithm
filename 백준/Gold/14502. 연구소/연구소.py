550fb980871a838ef830dac9d036fd1b6bd4ce1a
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-05-16 11:37:42 +0900

    [Gold IV] Title: 연구소, Time: 2660 ms, Memory: 34364 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,51 +1,47 @@
 import sys
-from collections import deque
 import copy
-
+from collections import deque
+from itertools import combinations
 input = sys.stdin.readline
 
 if __name__ == "__main__":
-    n,m = list(map(int,input().split()))
-    arr = [list(map(int,input().split())) for _ in range(n)] # n 세로, m 가로
-    res = 0
-
-    def checksafe():
-        global res
-        q = deque()
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-        tmparr = copy.deepcopy(arr)
-
+        n, m = map(int, input().split())
+        arr = [list(map(int, input().split())) for _ in range(n)]
+        visited = [[0] * m for _ in range(n)]
+        res = 0
+        walls = []
         for i in range(n):
             for j in range(m):
-                if tmparr[i][j] == 2:
-                    q.append([i,j])
-        while q:
-            x,y = q.popleft()
-            for k in range(4):
-                xx = x + dx[k]
-                yy = y + dy[k]
-                if 0<=xx<n and 0<=yy<m and tmparr[xx][yy] == 0:
-                    tmparr[xx][yy] = 2
-                    q.append([xx,yy])
-        tmpcnt = 0
-        for i in range(n):
-            for j in range(m):
-                if tmparr[i][j] == 0 :
-                    tmpcnt +=1
-        res = max(res,tmpcnt)
+                if not arr[i][j]:
+                    walls.append((i,j))
 
+        def spreadVirus(tmp_arr):
+            dx = [1, -1, 0, 0]
+            dy = [0, 0, 1, -1]
+            # 바이러스 퍼뜨리기
+            q = deque()
+            for i in range(n):
+                for j in range(m):
+                    if tmp_arr[i][j] == 2:
+                        q.append([i, j])
+            while q:
+                x, y = q.popleft()
+                for k in range(4):
+                    xx = x + dx[k]
+                    yy = y + dy[k]
+                    if 0 <= xx < n and 0 <= yy < m and not tmp_arr[xx][yy]:
+                        tmp_arr[xx][yy] = 2
+                        q.append([xx, yy])
 
-    def dfs(l):
-        if l == 3:
-            checksafe()
-            return
-        for i in range(n):
-            for j in range(m):
-                if arr[i][j] == 0 :
-                    arr[i][j] = 1
-                    dfs(l+1)
-                    arr[i][j] = 0
-    dfs(0)
-    print(res)
-                
+            # 안전영역구하기
+            safezone = 0
+            for i in range(n):
+                safezone += tmp_arr[i].count(0)
+            return safezone
+
+        for w in combinations(walls,3):
+            tmp_arr = copy.deepcopy(arr)
+            for x,y in w:
+                tmp_arr[x][y] = 1
+            res = max(res, spreadVirus(tmp_arr))
+        print(res)
