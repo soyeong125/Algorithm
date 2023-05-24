f51670216d303ce6c3cef5e61895689f2c217cfd
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-05-24 17:59:05 +0900

    [Silver II] Title: 꽃길, Time: 2396 ms, Memory: 126548 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -1,34 +1,47 @@
 import sys
 import copy
-from collections import deque
-from itertools import combinations
 input = sys.stdin.readline
 
 if __name__ == "__main__":
-    n = int(input())
-    area = [list(map(int,input().split())) for _ in range(n)]
-    combi = [(r,c) for r in range(1,n-1) for c in range(1,n-1)]
-    res = 10**8
-
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    def check(c):
-        global res
-        cost = 0
+        n = int(input())
+        res = 10**8
+        arr = [list(map(int,input().split())) for _ in range(n)]
         visited = [[0] * n for _ in range(n)]
-        for x,y in c:
-            visited[x][y] = 1
-            cost += area[x][y]
-            for k in range(4):
-                xx = x + dx[k]
-                yy = y + dy[k]
-                if not visited[xx][yy]:
-                    cost += area[xx][yy]
-                    visited[xx][yy] = 1
-                else:
-                    return
-        res = min(res,cost)
 
-    for c in combinations(combi,3):
-        check(c)
-    print(res)
+        def check():
+            tmp_arr = copy.deepcopy(visited)
+            seed = []
+            cnt = 0
+            dx = [1,-1,0,0]
+            dy = [0,0,1,-1]
+
+            for i in range(1,n-1):
+                for j in range(1,n-1):
+                    if tmp_arr[i][j]:
+                        seed.append([i,j])
+                        cnt += arr[i][j]
+            for x,y in seed:
+                for k in range(4):
+                    xx = x + dx[k]
+                    yy = y + dy[k]
+                    if not tmp_arr[xx][yy]:
+                        tmp_arr[xx][yy] = 1
+                        cnt += arr[xx][yy]
+                    else:
+                        return -1
+            return cnt
+        def dfs(l):
+            global res
+            if l == 3:
+                tmp = check()
+                if tmp >= 0:
+                    res = min(res,tmp)
+                return
+            for i in range(1,n-1):
+                for j in range(1,n-1):
+                    if not visited[i][j] and not visited[i-1][j] and not visited[i][j-1] and not visited[i+1][j] and not visited[i][j+1]:
+                        visited[i][j] = 1
+                        dfs(l+1)
+                        visited[i][j] = 0
+        dfs(0)
+        print(res)
