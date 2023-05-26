3310266c1d7bea71a24ea5771b14d9133931177d
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-05-26 11:35:35 +0900

    [Silver III] Title: 숫자 야구, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,24 +1,36 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    permulist = list(permutations([i for i in range(1,10)],3))
-    reslist = [1] * len(permulist)
+    paramlist = [list(map(int,input().split())) for _ in range(n)]
+    arrlist = ['1','2','3','4','5','6','7','8','9']
+    answer = []
+    res = 0
 
-    for i in range(n):
-        num,strike,ball = map(int,input().split())
-        for p in range(len(permulist)):
-            if not reslist[p]:
-                continue
+    def check():
+        global res
+        for i in range(len(paramlist)):
+            num,strike,ball = paramlist[i]
             s,b = 0,0
             for i in range(3):
-                if str(num)[i] == str(permulist[p][i]):
-                    s+=1
-                elif str(num)[i] in str(permulist[p]):
-                    b+=1
-            if s != strike or b != ball:
-                reslist[p] = 0
+                if int(str(num)[i]) == answer[i]:
+                    s += 1
+                elif int(str(num)[i]) in answer:
+                    b += 1
+            if strike != s or ball != b:
+                return
+        res += 1
+        return
 
-    print(reslist.count(1))
+    def dfs(l):
+        if l == 3:
+            check()
+            return
+        for i in range(1,10):
+            if i not in answer:
+                answer.append(i)
+                dfs(l+1)
+                answer.pop()
+    dfs(0)
+    print(res)
