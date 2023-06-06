d6c40b705e7bce5e1153cc2bd0ae4753057248db
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-06-06 14:19:36 +0900

    [Gold III] Title: 성곽, Time: 3720 ms, Memory: 157180 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.py
+++ 백준/Gold/2234. 성곽/성곽.py
@@ -11,46 +11,49 @@ if __name__ == "__main__":
             changeVal = bin(room_input[i][j])[2:]
             room_2[i].append(list(map(int,'0'*(4-len(changeVal))+changeVal)))
 
-    def bfs():
-        visited = [[0] * m for _ in range(n)]
-        res = []
-        cnt1,cnt2 =0,0 #방의 갯수, 방의 넓이
+    def bfs(X,Y):
         dx = [1,0,-1,0]
         dy = [0,1,0,-1]
-        for i in range(n):
-            for j in range(m):
-                if not visited[i][j]:
-                    visited[i][j] = 1
-                    q = deque()
-                    q.append([i,j])
-                    cnt1 +=1
-                    cnt2 = 1
-                    while q:
-                        x,y = q.popleft()
-                        for kk in range(4):
-                            if not room_2[x][y][kk]:
-                                xx = x + dx[kk]
-                                yy = y + dy[kk]
-                                if 0<=xx<n and 0<=yy<m and not visited[xx][yy]:
-                                    q.append([xx,yy])
-                                    visited[xx][yy] = 1
-                                    cnt2 +=1
-                    res.append([cnt1,cnt2])
-        return res
-    tmp1 = bfs()
-    tmp1.sort(key = lambda x:-x[1])
-    maxRoomSpace = 0
 
+        q = deque()
+        q.append([X, Y])
+        cnt = 1
+
+        while q:
+            x, y = q.popleft()
+            for kk in range(4):
+                if not room_2[x][y][kk]:
+                    xx = x + dx[kk]
+                    yy = y + dy[kk]
+                    if 0 <= xx < n and 0 <= yy < m and not visited[xx][yy]:
+                        q.append([xx, yy])
+                        visited[xx][yy] = 1
+                        cnt += 1
+        return cnt
+
+    visited = [[0] * m for _ in range(n)]
+    res1,res2 = 0,0
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j]:
+                visited[i][j] = 1
+                res1 +=1
+                res2 = max(bfs(i,j),res2)
+
+    res3 = 0
     for i in range(n):
         for j in range(m):
             for k in range(2):
                 if room_2[i][j][k]:
                     room_2[i][j][k] = 0
-                    tmp = bfs()
-                    tmp.sort(key = lambda x:-x[1])
-                    maxRoomSpace = max(maxRoomSpace, tmp[0][1])
+                    visited = [[0] * m for _ in range(n)]
+                    for xx in range(n):
+                        for yy in range(m):
+                            if not visited[xx][yy]:
+                                visited[xx][yy] = 1
+                                res3 = max(bfs(xx,yy),res3)
                     room_2[i][j][k] = 1
 
-    print(len(tmp1))
-    print(tmp1[0][1])
-    print(maxRoomSpace)
+    print(res1)
+    print(res2)
+    print(res3)
