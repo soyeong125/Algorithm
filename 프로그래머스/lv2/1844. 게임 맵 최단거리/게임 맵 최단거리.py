fe9567ef93fafee18cfe7bfed1d2e2a4b251b4ff
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-06-08 08:58:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.53 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,32 @@
+from collections import deque
+def solution(maps):
+    n = len(maps)
+    m = len(maps[0])
+    answer = 0
+    visited = [[0] * (m) for _ in range(n)]
+    
+    q = deque()
+    q.append([0,0,1])
+    visited[0][0] = 1
+    result = -1
+    
+    dx = [1,-1,0,0]
+    dy =[0,0,1,-1]
+    
+    while q:
+        x,y,cnt = q.popleft()
+        if x == (n-1) and y == (m-1):
+            result = cnt
+            break
+        for k in range(4):
+            xx = x + dx[k]
+            yy = y + dy[k]
+            if 0<=xx<n and 0<=yy<m and maps[xx][yy]:
+                if not visited[xx][yy]:
+                    visited[xx][yy] = 1
+                    q.append([xx,yy,cnt+1])
+    if result == -1:
+        answer = -1
+    else:
+        answer = result
+    return answer
