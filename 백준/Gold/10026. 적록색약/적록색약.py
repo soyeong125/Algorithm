1286a6c1ec6fba33b9f36e9b9f1eace553e936ec
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-06-26 09:24:23 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,64 +1,43 @@
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(100000)
-
+from collections import deque
 if __name__ == "__main__":
-    n = int(input())
-    area = [list(map(str,input())) for _ in range(n)]
-  
+    if __name__ == "__main__":
+        n = int(input())
+        arr = [list(input().strip()) for _ in range(n)]
+
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
+
+        def bfs(tmp,is_normal):
+            cnt = 0
+            visited = [[0] * n for _ in range(n)]
+            for i in range(n):
+                for j in range(n):
+                    if not visited[i][j]:
+                        cnt += 1
+                        visited[i][j] = 1
+                        q = deque()
+                        q.append([i, j])
+                        while q:
+                            x, y = q.popleft()
+                            for k in range(4):
+                                xx = x + dx[k]
+                                yy = y + dy[k]
+                                if 0 <= xx < n and 0 <= yy < n and not visited[xx][yy]:
+                                    if is_normal:
+                                        if tmp[x][y] == tmp[xx][yy]:
+                                            q.append([xx, yy])
+                                            visited[xx][yy] = 1
+                                    else:
+                                        if (tmp[x][y] == 'R' or tmp[x][y] == 'G') and (tmp[xx][yy] == 'R' or tmp[xx][yy] == 'G'):
+                                            q.append([xx,yy])
+                                            visited[xx][yy] = 1
+                                        elif tmp[x][y] == tmp[xx][yy]:
+                                            q.append([xx,yy])
+                                            visited[xx][yy] = 1
+
+            return cnt
 
 
-    def DFS1(x,y,cur):
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-        for k in range(4):
-            xx = x + dx[k]
-            yy = y + dy[k]
-            if 0<= xx < n and 0<= yy < n and not visited[xx][yy]:
-                if area[xx][yy] == cur:
-                    visited[xx][yy] = 1
-                    DFS1(xx,yy,cur)
-    def DFS2(x,y,cur):
-        dx = [1,-1,0,0]
-        dy = [0,0,1,-1]
-        for k in range(4):
-            xx = x + dx[k]
-            yy = y + dy[k]
-            if 0<= xx < n and 0<= yy < n and not visited[xx][yy]:
-                if cur == 'R' or cur == 'G':
-                    if area[xx][yy] == 'R' or area[xx][yy] == 'G':
-                        visited[xx][yy]=1
-                        DFS2(xx,yy,cur)
-                else:
-                    if area[xx][yy] == cur:
-                        visited[xx][yy]=1
-                        DFS2(xx,yy,cur)
-
-            
-
-
-    cnt1 = 0
-    visited = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j]:
-                visited[i][j] = 1
-                cnt1 +=1
-                DFS1(i,j,area[i][j])
-
-    cnt2 = 0
-    visited = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j]:
-                visited[i][j] = 1
-                cnt2 +=1
-                DFS2(i,j,area[i][j])
-
-    print(cnt1,cnt2)             
-                
- 
-
-
-
-    
+        print(bfs(arr,True))
+        print(bfs(arr,False))
