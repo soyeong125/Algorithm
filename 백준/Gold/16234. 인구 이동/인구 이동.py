93e3bef189bffed1b51cd00936460c91b7ba28c5
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-06-26 09:55:50 +0900

    [Gold V] Title: 인구 이동, Time: 5392 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,58 +1,53 @@
 import sys
-import math
 from collections import deque
 
-# 남 동 북 서
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+input = sys.stdin.readline
 
-n, l, r = map(int, sys.stdin.readline().split())  # n*n, 인구차이 l명 이상, r명 이하
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
-arr = list()
-a_list = list()
-for i in range(n):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-
-def bfs(i, j):
-    dq = deque()
-    dq.append((i, j))
-    visit[i][j] = True
-    # 연합된 국가 담기
-    union = [(i, j)]
-    count = arr[i][j]   # 총 연합된 국가 수 
-        # 1. 인접 국가를 탐색하면서 인구차이 l명 이상, r명 이하인 경우 연합 국가에 담기 
-    while dq:
-        x, y = dq.popleft()
+def bfs(sy, sx):
+    population = lst[sy][sx]  # 시작 나라의 인구수
+    union = [(sy, sx)]  # 연합에 속한 나라의 좌표
+    visit[sy][sx] = True  # 방문 여부 표시
+    queue = deque([(sy, sx)])  # BFS를 위한 큐
+    while queue:
+        y, x = queue.popleft()
         for d in range(4):
-            nx = x + dx[d]
             ny = y + dy[d]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            nx = x + dx[d]
+            if ny < 0 or nx < 0 or ny >= N or nx >= N:
                 continue
-            if visit[nx][ny]:
+            if visit[ny][nx]:
                 continue
-            if l <= abs(arr[nx][ny] - arr[x][y]) <= r:  # 인구차이 l명 이상, r명 이하인 경우, 연합 국가에 담기 
-                union.append((nx, ny))
-                visit[nx][ny] = True
-                dq.append((nx, ny))
-                count += arr[nx][ny]
-    # 2. 연합 국가 간 인구수는 (연합의 인구수) / (연합을 이루고 있는 칸의 개수) 가 되도록 계산  
-    for x, y in union:
-        arr[x][y] = math.floor(count / len(union))
+            diff = abs(lst[y][x] - lst[ny][nx])
+            if diff < L or diff > R:
+                continue
+            visit[ny][nx] = True
+            population += lst[ny][nx]
+            union.append((ny, nx))
+            queue.append((ny, nx))
+    
+    if len(union) > 1:
+        avg = population // len(union)  # 연합의 평균 인구수
+        for y, x in union:
+            lst[y][x] = avg  # 연합의 인구수 갱신
+    return len(union) > 1
 
-    return len(union)
+N, L, R = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
 
-result = 0    # 인구 이동이 발생하는 일수 
-while True:   # 1. 인구 이동이 없을 때까지 반복 
-    visit = [[False] * n for _ in range(n)]
-    flag = False  # 인구 이동 존재 유무 플래그 
-    # 2. 모든 곳을 bfs로 방문하여 연합 진행 
-    for i in range(n):
-        for j in range(n):
-            if not visit[i][j]:
-                if bfs(i, j) > 1:
-                    flag = True
-    if not flag:   # 3. 지금까지 인구 이동이 없는 경우, 그만 
+day = 0  # 인구 이동이 발생하는 일수
+while True:
+    visit = [[False] * N for _ in range(N)]  # 방문 여부 초기화
+    moved = False  # 인구 이동이 발생했는지 여부
+    for y in range(N):
+        for x in range(N):
+            if not visit[y][x]:
+                if bfs(y, x):
+                    moved = True
+    if not moved:  # 인구 이동이 더 이상 발생하지 않으면 종료
         break
-    result += 1
+    day += 1
 
-print(result)
+print(day)
