da55d62eed9c3ea88c1aaf36acf7bb392e0109b1
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-07-07 15:39:55 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,4 +1,5 @@
 import math
+
 def solution(fee, records):
     answer = []
 
@@ -6,15 +7,13 @@ def solution(fee, records):
     sort_records.sort(key=lambda x: (x[1], x[0])) # 차랑변호, 날짜순으로 정렬
 
 
-    start_time = sort_records[0][0] # 탐색할 첫번째 차의 입차 시간
-    end_time = ""
+    start_time, car = sort_records[0][0], sort_records[0][1] # 탐색할 첫번째 차의 입차 시간과, 차량 번호
+    end_time = "23:59"
     total_time = 0
 
     def cost(st,et,tt): # 비용 계산
-        if et == "":  # 출차 시간이 없으면 23:59에 출차한 걸로 초기화
-            et = "23:59"
+        if et == "23:59":
             tt += ((int(et[:2]) - int(st[:2])) * 60) + (int(et[3:]) - int(st[3:]))
-
         # 이전 차 최종 주차 요금 계산
         if tt <= fee[0]:  # 기본 시간 이하로 있는 경우
             answer.append(fee[1])
@@ -23,11 +22,10 @@ def solution(fee, records):
 
     #1번부터 탐색 시작
     for idx in range(1,len(sort_records)):
-        if sort_records[idx-1][1] == sort_records[idx][1]: # 차량 번호가 같으면
+        if car == sort_records[idx][1]: # 이전 차와 차량 번호가 같으면 주차 총 시간 계산해주기
             if sort_records[idx][2] == "OUT": # 출차한 경우, 종료 시간 체크하고 총 소요시간 계산
                 end_time = sort_records[idx][0]
                 total_time += ((int(end_time[:2]) - int(start_time[:2])) * 60) + (int(end_time[3:]) - int(start_time[3:]))
-
 
         else: #차량 번호가 다르면 == 다음차 탐색이 시작되면
             cost(start_time, end_time, total_time)
@@ -36,8 +34,9 @@ def solution(fee, records):
         # 입차 인 경우 입차 시간 초기화, 출차 시간 초기화
         if sort_records[idx][2] == "IN":
             start_time = sort_records[idx][0]
-            end_time = ""
+            end_time = "23:59"
 
+        car = sort_records[idx][1] #현재 기준이 되는 차 변경
 
     cost(start_time, end_time, total_time) # 가장 마지막 차의 최종 출차 시간 체크 및 주차 비용 체크
 
