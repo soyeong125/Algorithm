f4ade210393bbc966090186bbfe7998ac8c879fd
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-08-03 21:39:14 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 40292 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,19 +1,8 @@
 if __name__ == "__main__":
     n, k = map(int,input().split())
     arr = list(map(int,input().split()))
+    res = [sum(arr[:k])]
 
-
-    sum_val = sum(arr[0:k])
-    res = sum_val
-    rm = 0
-    include = k
-
-
-    while include < n :
-        tmp_val = sum_val - arr[rm] + arr[include]
-        res = max(res,tmp_val)
-        sum_val = tmp_val
-        rm += 1
-        include +=1
-
-    print(res)
+    for i in range(n-k):
+        res.append(res[i] - arr[i] + arr[k+i])
+    print(max(res))
