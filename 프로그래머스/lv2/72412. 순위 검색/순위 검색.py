d6d8d6f6f9c410c0df0e92dd834077397ee0cf44
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-08-04 23:47:21 +0900

    [level 2] Title: 순위 검색, Time: 1008.80 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,24 +1,35 @@
-import bisect, itertools, collections
+
+import collections
 def solution(info, query):
         infomap = collections.defaultdict(list)
-        binarys = list(itertools.product((True, False), repeat=4))
-
         #하나의 info에서 나올 수 있는 16가지의 key를 만들어서 infomap[key]에 값을 추가해줍니다.
         for inf in info:
-            inf = inf.split()
-            for binary in binarys:
-                key = ''.join([inf[i] if binary[i] else '-' for i in range(4)])
-                infomap[key].append(int(inf[4]))
+            i = inf.split()
+            for a in [i[0],'-']:
+                for b in [i[1], '-']:
+                    for c in [i[2], '-']:
+                        for d in [i[3],'-']:
+                            infomap[(a,b,c,d)].append(int(i[4]))
+
         #이분탐색을 위해 infomap의 값들을 정렬합니다.
-        for k in infomap.keys():
+        for k in infomap:
             infomap[k].sort()
 
-        #query의 값을 key로 만들고 이분탐색으로 point 이상의 값 개수를 구합니다.
+        # 이분탐색으로 point 이상의 값 개수를 구합니다.
         answers = []
         for q in query:
             l, _, p, _, c, _, f, point = q.split()
-            key = ''.join([l, p, c, f])
-            i = bisect.bisect_left(infomap[key], int(point))
-            answers.append(len(infomap[key]) - i)
+            key = infomap[(l, p, c, f)]
+
+            l,r = 0,len(key)
+
+            while l < r:
+                mid = (l+r) //2
+                if key[mid] >= int(point):
+                    r = mid 
+                else:
+                    l = mid+1
+
+            answers.append(len(key) - l)
 
         return(answers)
