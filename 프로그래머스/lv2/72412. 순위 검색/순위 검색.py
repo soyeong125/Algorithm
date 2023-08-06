79833ab8cdf8eb9ec714d58ba0a1e9734eb213d3
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-08-06 23:27:21 +0900

    [level 2] Title: 순위 검색, Time: 911.81 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,21 +1,12 @@
 
 import collections
 def solution(info, query):
-        #infomap[key]에 값을 추가해줍니다.
-        infomap = collections.defaultdict(list)     
-        
-        # for inf in info:
-        #     a,b,c,d,p = inf.split()
-        #     infomap[''.join([a,b,c,d])].append(int(p))
-        # 지원자의 정보로 가능한 모든 조합의 키를 생성해야 한다.
+        infomap = collections.defaultdict(list)
+       #infomap[key]에 값을 추가해줍니다.
         for inf in info:
-            a, b, c, d, p = inf.split()
-        # 가능한 모든 조합 생성
-            for l in [a, '-']:
-                for m in [b, '-']:
-                    for n in [c, '-']:
-                        for o in [d, '-']:
-                            infomap[l+m+n+o].append(int(p))        
+            a,b,c,d,p = inf.split()
+            infomap[''.join([a,b,c,d])].append(int(p))
+                
 
         #이분탐색을 위해 infomap의 값들을 정렬합니다.
         for k in infomap:
@@ -25,17 +16,17 @@ def solution(info, query):
         answers = []
         for q in query:
             l, _, p, _, c, _, f, point = q.split()
-            score_list = infomap[l + p + c + f]
+            key = infomap[''.join([l, p, c, f])]
 
-            l,r = 0,len(score_list)
+            l,r = 0,len(key)
 
             while l < r:
                 mid = (l+r) //2
-                if score_list[mid] >= int(point):
-                    r = mid
+                if key[mid] >= int(point):
+                    r = mid-1
                 else:
                     l = mid+1
 
-            answers.append(len(score_list) - l)
+            answers.append(len(key) - l)
 
         return(answers)
