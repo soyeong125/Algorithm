0cb01525660f3887cda77e141e8ff446968ea9b6
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-08-06 23:26:36 +0900

    [level 2] Title: 순위 검색, Time: 911.81 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,35 +1,41 @@
 
 import collections
 def solution(info, query):
-        infomap = collections.defaultdict(list)
-        #하나의 info에서 나올 수 있는 16가지의 key를 만들어서 infomap[key]에 값을 추가해줍니다.
+        #infomap[key]에 값을 추가해줍니다.
+        infomap = collections.defaultdict(list)     
+        
+        # for inf in info:
+        #     a,b,c,d,p = inf.split()
+        #     infomap[''.join([a,b,c,d])].append(int(p))
+        # 지원자의 정보로 가능한 모든 조합의 키를 생성해야 한다.
         for inf in info:
-            i = inf.split()
-            for a in [i[0],'-']:
-                for b in [i[1], '-']:
-                    for c in [i[2], '-']:
-                        for d in [i[3],'-']:
-                            infomap[(a,b,c,d)].append(int(i[4]))
+            a, b, c, d, p = inf.split()
+        # 가능한 모든 조합 생성
+            for l in [a, '-']:
+                for m in [b, '-']:
+                    for n in [c, '-']:
+                        for o in [d, '-']:
+                            infomap[l+m+n+o].append(int(p))        
 
         #이분탐색을 위해 infomap의 값들을 정렬합니다.
         for k in infomap:
             infomap[k].sort()
 
-        # 이분탐색으로 point 이상의 값 개수를 구합니다.
+        # 이분탐색으로 point 이상의 값 개수를 구합니다.(효율성)
         answers = []
         for q in query:
             l, _, p, _, c, _, f, point = q.split()
-            key = infomap[(l, p, c, f)]
+            score_list = infomap[l + p + c + f]
 
-            l,r = 0,len(key)
+            l,r = 0,len(score_list)
 
             while l < r:
                 mid = (l+r) //2
-                if key[mid] >= int(point):
-                    r = mid 
+                if score_list[mid] >= int(point):
+                    r = mid
                 else:
                     l = mid+1
 
-            answers.append(len(key) - l)
+            answers.append(len(score_list) - l)
 
         return(answers)
