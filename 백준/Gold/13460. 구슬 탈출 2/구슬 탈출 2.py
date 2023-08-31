42879f8c5302630d163273d2a7466e9b96401716
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-08-31 12:01:00 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 64 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -3,66 +3,69 @@ from collections import deque
 
 if __name__ == "__main__":
     n,m = map(int,input().split())
-    board = [list(input().rstrip()) for _ in range(n)]
-
+    arr = [list(input().rstrip()) for _ in range(n)]
     res = -1
-    #구슬의 위치 찾기
+
     rx,ry,bx,by = 0,0,0,0
+
     for i in range(n):
         for j in range(m):
-            if board[i][j] == 'R':
-                rx,ry = i,j
-            elif board[i][j] == 'B':
-                bx,by = i,j
+            if arr[i][j] == 'R':
+                rx , ry = i ,j
+            elif arr[i][j] == 'B':
+                bx,by = i, j
 
-    q = deque()
-    q.append([rx,ry,bx,by,0])
+    qq = deque()
+    qq.append([rx,ry,bx,by,0])
+
     visited = [[[[0] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
     visited[rx][ry][bx][by] = 1
 
-    dx = [1,-1,0,0]
+    dx = [1,-1,0,0] #아래 위 오 왼
     dy = [0,0,1,-1]
 
-    def move(x,y,dxx,dyy):
-        tmp_cnt = 0 #이동 횟수 체크
+    def move(x,y,k):
+        tmp_cnt = 0
 
-        while board[x+dxx][y+dyy] != '#' and board[x][y] != 'O': #벽이 아니고 현재 0이 아니면
-                x += dxx
-                y += dyy
-                tmp_cnt +=1
+        while arr[x+dx[k]][y+dy[k]] != '#' and arr[x][y] != 'O': #다음에 갈 수 있고 & 현재 구멍이 아니면 계속 이동
+            x += dx[k]
+            y += dy[k]
+            tmp_cnt +=1
 
-        return x,y,tmp_cnt #최종 값 리턴
+        return [x,y,tmp_cnt]
 
-    flag = True
-    while q:
-        rx1,ry1,bx1,by1,cnt = q.popleft()
 
-        if cnt >= 10: #움직인 횟수가 10번 이상이면 실패
-            break
-
+    while qq:
+        rx,ry,bx,by,cnt = qq.popleft()
+        if cnt >= 10:
+            continue
         for k in range(4):
-            rx2,ry2,rcnt = move(rx1,ry1,dx[k],dy[k])
-            bx2,by2,bcnt = move(bx1,by1,dx[k],dy[k])
+            #빨간 구슬 움직이기
+            rxx,ryy,r_cnt = move(rx,ry,k)
+            #파란 구슬 움직이기
+            bxx,byy,b_cnt = move(bx,by,k)
 
-            if board[bx2][by2] == 'O':  # 파란색이 먼저 구멍에 들어가면 실패
+            #파란색이 빠졌으면 fail
+            if arr[bxx][byy] == 'O':
                 continue
-            if board[rx2][ry2] == 'O':  # 빨간 구술이 구멍에 들어가면 성공
+            #빨간색이 빠졌으면 cnt + 1 리턴하고 끝
+            if arr[rxx][ryy] == 'O':
                 res = cnt + 1
-                flag = False
                 break
 
-            #빨간 구슬하고 파란구슬하고 겹친다면 cnt가 더 많은 구슬을 하나 뒤로 빼준다
-            if rx2 == bx2 and ry2 == by2:
-                if rcnt > bcnt:
-                    rx2 -= dx[k]
-                    ry2 -= dy[k]
+            #둘이 동일한 위치라면 더 많이 움직인 쪽이 한칸 뒤로 가기
+            if rxx == bxx and ryy == byy:
+                if r_cnt > b_cnt:
+                    rxx -= dx[k]
+                    ryy -= dy[k]
                 else:
-                    bx2 -= dx[k]
-                    by2 -= dy[k]
+                    bxx -= dx[k]
+                    byy -= dy[k]
 
-            if not visited[rx2][ry2][bx2][by2]:
-                q.append([rx2,ry2,bx2,by2,cnt+1])
-                visited[rx2][ry2][bx2][by2] = 1
-        if not flag:
+            #신규 위치 q에 넣기
+            if visited[rxx][ryy][bxx][byy] == 0:
+                visited[rxx][ryy][bxx][byy] = 1
+                qq.append([rxx,ryy,bxx,byy,cnt+1])
+        if res > 0 :
             break
     print(res)
