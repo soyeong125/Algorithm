295caa2ad97e7da29acafd3070ff4e41e481a6bb
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-09-01 12:42:38 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 2.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,35 +1,31 @@
 from collections import deque
 def solution(n, m, x, y, r, c, k):
     answer = 'impossible'
-    #visited 체크 안해도됨 (해야됨 시간초과 어떻게 해결하지?)
-    #bfs사용(x,y,cnt) cnt k이상이면 날려버리기
-    #bfs 계속해서 cnt == k 이고 x,y가 r,c면 reslist에 넣기
-    #reslist 갯수가 0 이면 임파서블 출력
-    #reslist 정렬 0번째가 정답
-    q = deque()
-    q.append([x,y,0,''])
+
+    dd = ["d","l","r","u"]
     dx = [1,0,0,-1]
     dy = [0,-1,1,0]
-    dic = {0:'d',1:'l',2:'r',3:'u'}
-
-
+    
+    q = deque()
+    q.append([x,y,0,""])
+    
     while q:
-        xx,yy,cnt,arr = q.popleft()
-        if cnt > k:
-            continue
-        if cnt == k:
-            if (xx == r and yy == c):
-                answer = arr
-                break
-        for s in range(4):
-            sx = xx + dx[s]
-            sy = yy + dy[s]
-            if sx <= 0 or sx > n or sy <=0 or sy > m:
-                continue
-            if abs(sx - r) + abs(sy - c) + cnt + 1 > k:
-                continue
-            q.append([sx,sy,cnt+1,arr+dic[s]])
+        x1,y1,cnt,tmp = q.popleft()
+        if (x1 == r and y1 == c) and cnt == k:
+            answer = tmp
             break
-                
-                   
+            
+        if cnt >= k:
+            continue
+            
+        for kk in range(4):
+            xx = x1 + dx[kk]
+            yy = y1 + dy[kk]
+            if 0<xx<=n and 0<yy<=m:
+                if abs(xx-r) + abs(yy-c) + cnt + 1 <= k:
+                    q.append([xx,yy,cnt+1,tmp+dd[kk]])
+                    break
+                               
     return answer
+
+
