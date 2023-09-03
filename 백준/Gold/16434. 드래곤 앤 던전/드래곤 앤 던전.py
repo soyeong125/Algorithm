8efc49677f4cca2a422ef704e7cfd8ffbadb67f0
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-09-03 12:50:19 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 3628 ms, Memory: 57920 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -2,32 +2,32 @@ import sys
 import math
 
 if __name__ == "__main__":
-    # 방의 개수, 초기 공격력
-    n, att = map(int, input().split())
-    room = []
-    for _ in range(n):
-        line = list(map(int, sys.stdin.readline().split()))
-        room.append(line)
+    n,n_atk = map(int,input().split())
+    rooms = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-    # 용사의 최소, 최대 hp
-    left, right = 1, int(1e18)
-    answer = 0
-    while left <= right:
-        mid = (left + right) // 2
-        # 용사가 던전에 입장할때 hp, 공격력
-        hp, attack = mid, att
-        for r in room:
-            t, a, h = r[0], r[1], r[2]
+    max_hp = 0
+    l , r = 1, int(1e18)
+
+    while l <= r:
+        mid = (l+r) // 2
+        cur_hp = mid
+        atk = n_atk
+
+        for room in rooms:
+            t, a, h = room[0], room[1], room[2]
             if t == 1:
-                hp -= (math.ceil(h / attack) - 1) * a
-                if hp <= 0:
+                cur_hp -= (math.ceil(h / atk) - 1) * a
+                if cur_hp <= 0:
                     break
+
             else:
-                attack += a
-                hp = min(mid, hp + h)
-        if hp > 0:
-            right = mid - 1
-            answer = mid
+                atk += a
+                cur_hp = min(mid, cur_hp + h)
+
+        if cur_hp <= 0:
+            l = mid + 1
         else:
-            left = mid + 1
-    print(answer)
+            r = mid - 1
+            max_hp = mid
+
+    print(max_hp)
