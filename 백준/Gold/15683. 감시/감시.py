7e7998a15f936db0b1fc7137260ab0c3b8119f55
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-09-17 12:31:04 +0900

    [Gold IV] Title: 감시, Time: 2776 ms, Memory: 38972 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.py
@@ -0,0 +1,65 @@
+import sys
+import itertools
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+    # CCTV 개수 확인
+    cctv_cnt = 0
+    cctv = []
+    wall = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] > 0 and arr[i][j] < 6:
+                cctv_cnt += 1
+                cctv.append([i, j])
+            if arr[i][j] == 6:
+                wall += 1
+
+
+    cctv_list = list(itertools.product([i for i in range(4)], repeat=cctv_cnt))
+
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    direction = {
+        1 : [[0],[1],[2],[3]],
+        2 : [[0,1],[2,3],[0,1],[2,3]],
+        3 : [[0,2],[0,3],[1,2],[1,3]],
+        4 : [[0,1,2],[0,1,3],[0,2,3],[1,2,3]],
+        5 : [0,1,2,3]
+    }
+
+
+    res = int(1e9)
+    for cl in cctv_list:
+        visited = [[0] * m for _ in range(n)]
+
+        for i in range(cctv_cnt):
+            cctv_num = arr[cctv[i][0]][cctv[i][1]]
+            if cctv_num == 5:
+                cctv_dir = direction[cctv_num]
+            else:
+                cctv_dir = direction[cctv_num][cl[i]]
+
+            for d in cctv_dir:
+                x = cctv[i][0]
+                y = cctv[i][1]
+                xx = dx[d]
+                yy = dy[d]
+                while 0 <= x + xx < n and 0 <= y + yy < m:
+                    if arr[x + xx][y + yy] == 6:
+                        break
+                    x += xx
+                    y += yy
+                    if visited[x][y] == 0 and arr[x][y] == 0:
+                        visited[x][y] = 1
+
+        v_cnt = 0
+        for h in range(n):
+            v_cnt += visited[h].count(1)
+        tmp_res = max(0, (n * m) - wall - v_cnt - cctv_cnt)
+        res = min(res, tmp_res)
+
+    print(res)
