712bcbbf5ac93710e82c19e0a5ffe28c8ad593bd
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-04 20:11:49 +0900

    [unrated] Title: 문자열 출력하기, Time: 164.35 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,14 +1,10 @@
 import java.util.*;
-import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        System.out.println(line);
-        
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println(sc.nextLine());
+    public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         String a = sc.nextLine();
+         System.out.println(a);
             
     }
 }
