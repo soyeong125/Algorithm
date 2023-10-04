b4f82551362a6c8a8a4e0ae16ea555136e75ae3f
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-04 22:39:26 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,9 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[n];
-        for (int i = 0 ; i < n ; i++){
-            answer[i] = num_list[i];
-        }
+        int[] answer = {};
+        answer = Arrays.copyOfRange(num_list,0,n);
         return answer;
     }
 }
