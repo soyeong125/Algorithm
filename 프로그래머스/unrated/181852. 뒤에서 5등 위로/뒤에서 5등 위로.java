0de0dd37d6ee5fed7550d010b799b24efce7c7a2
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-04 23:34:32 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.50 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -3,10 +3,10 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length - 5];
         Arrays.sort(num_list);
-        
-        for (int i = 5 ; i < num_list.length ; i++){
-            answer[i-5] = num_list[i];
-        }
+        answer = Arrays.copyOfRange(num_list,5,num_list.length);
+        // for (int i = 5 ; i < num_list.length ; i++){
+        //     answer[i-5] = num_list[i];
+        // }
         
         return answer;
     }
