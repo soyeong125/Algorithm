e92c5a92e99ff58d2c1523f19e62c11f5ea0a4d3
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-04 23:47:18 +0900

    [unrated] Title: rny_string, Time: 1.62 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,6 +1,28 @@
 class Solution {
     public String solution(String rny_string) {
-        String answer = rny_string.replaceAll("m","rn");
+        String answer = "";
+        String[] ch = rny_string.split("");
+        for(String s : ch){
+            if(s.equals("m")){
+                answer += "rn";
+            }
+            else{
+                answer += s;
+            }
+        }
         return answer;
     }
 }
+// class Solution {
+//     public String solution(String rny_string) {
+//         String answer = rny_string;
+//         String[] arr = answer.split("");
+//         for(int i = 0 ; i <= arr.length-1 ; i++) {
+//             if (arr[i].equals("m")) {
+//                 arr[i] = "rn";
+//             }
+//         }
+//         answer =  String.join("",arr);
+//         return answer;
+//     }
+// }
