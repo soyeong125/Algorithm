b73c09279cd45d2fb1e6cd368ae2e6e04298149d
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-04 23:09:32 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        // for(char c : myString.toCharArray()){
+        //     answer += Character.toUpperCase(c);
+        // }
+        return answer;
+    }
+}
