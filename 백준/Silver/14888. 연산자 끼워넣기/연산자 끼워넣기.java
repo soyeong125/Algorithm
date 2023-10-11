7d0298e073d1890cc47b18176e11dc5c9476d2ca
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-11 12:49:52 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 216 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,54 +4,31 @@ import java.util.stream.Collectors;
 
 public class Main {
     static int n;
-    static int m;
     static int[] arr;
     static int[] op_arr = new int[4];
-    static List<Integer> opList = new ArrayList<>();
-    static boolean[] visited;
-    static HashMap<Integer, String> ops;
-
     static int max_res = Integer.MIN_VALUE;
     static int min_res = Integer.MAX_VALUE;
 
-    static void dfs(int start, int end, int cnt) {
+    static void dfs(int num, int cnt) {
         if (cnt == n) {
-            max_res = Math.max(start, max_res);
-            min_res = Math.min(start, min_res);
+            max_res = Math.max(num, max_res);
+            min_res = Math.min(num, min_res);
             return;
         }
 
-        for (int i = 0; i < m; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                String o = ops.get(opList.get(i));
-                int tmp = 0;
-                switch (o) {
-                    case "+":
-                        tmp = start + end;
-                        break;
-                    case "-":
-                        tmp = start - end;
-                        break;
-                    case "x":
-                        tmp = start * end;
-                        break;
-                    case "/":
-                        if (start < 0) {
-                            tmp = (Math.abs(start) / end) * -1;
-                        } else {
-                            tmp = start / end;
-                        }
-                        break;
-                }
-                int tmp_end = end;
-                if (cnt + 1 < n) {
-                    end = arr[cnt + 1];
-                }
-                dfs(tmp, end, cnt + 1);
+        for (int i = 0; i < 4; i++) {
+            if (op_arr[i] > 0) {
+                op_arr[i] -= 1;
 
-                end = tmp_end;
-                visited[i] = false;
+                switch(i){
+                    case 0: dfs(num + arr[cnt] , cnt +1); break;
+                    case 1: dfs(num - arr[cnt] , cnt +1); break;
+                    case 2: dfs(num * arr[cnt] , cnt +1); break;
+                    case 3: dfs(num / arr[cnt] , cnt +1); break;
+                }
+
+                op_arr[i] += 1;
+
             }
         }
 
@@ -59,7 +36,7 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
 
@@ -68,31 +45,12 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        ops = new HashMap<>() {
-            {
-                put(0, "+");
-                put(1, "-");
-                put(2, "x");
-                put(3, "/");
-
-            }
-        };
 
         for (int i = 0; i < 4; i++) {
             op_arr[i] = sc.nextInt();
-            if (op_arr[i] > 0) {
-                int tmp = 0;
-                while (tmp < op_arr[i]) {
-                    opList.add(i);
-                    tmp += 1;
-                }
-            }
         }
 
-        m = opList.size();
-        visited = new boolean[m];
-
-        dfs(arr[0], arr[1], 1);
+        dfs(arr[0],1);
 
         System.out.println(max_res);
         System.out.println(min_res);
