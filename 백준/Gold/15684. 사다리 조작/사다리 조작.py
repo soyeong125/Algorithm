4925895152ea498119b4859c1a97103f15bd2ab5
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-10-18 13:41:00 +0900

    [Gold III] Title: 사다리 조작, Time: 4824 ms, Memory: 114028 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,15 +1,13 @@
 import sys
 input = sys.stdin.readline
-
 if __name__ == "__main__":
-    #시작점이 사다리를 타고 도착점에 도달했을 때 시작점과 동일한지 체크
-    def chk():
+    def check():
         for i in range(1,n+1):
             idx = i
             for j in range(1,h+1):
-                if graph[j][idx]:
+                if ladder[j][idx]:
                     idx +=1
-                elif graph[j][idx-1]:
+                elif ladder[j][idx-1]:
                     idx -=1
             if not idx == i:
                 return False
@@ -18,36 +16,40 @@ if __name__ == "__main__":
 
     def dfs(cnt,x):
         global answer
+
         if cnt > 3:
             return
-        if chk():
-            if answer > 0:
-                answer = min(cnt,answer)
+
+        if check():
+            if answer > 0 :
+                answer = min(cnt, answer)
             else:
                 answer = cnt
             return
-        # 연속되지 않는 가로선 그리기
+
+
+
         for i in range(x,h+1):
             for j in range(1,n):
-                if not graph[i][j]:
-                    if j-1 >= 0 and graph[i][j-1]:
+                if not ladder[i][j]:
+                    if j-1 >= 0 and ladder[i][j-1]:
                         continue
-                    if j+1 < n and graph[i][j+1]:
+                    if j+1 < n and ladder[i][j+1]:
                         continue
-                    graph[i][j] = 1
+                    ladder[i][j] = 1
                     dfs(cnt+1,i)
-                    graph[i][j] = 0
+                    ladder[i][j] = 0
 
     n,m,h = map(int,input().split())
-    graph = [[0] * (n + 1) for _ in range(h + 1)]
+    ladder = [[0] * (n+1) for _ in range(h+1)]
 
-    if not m: #입력되는 가로선이 없으면 바로 종료
+    if not m :
         print(0)
-        sys.exit()
+        exit()
 
     for _ in range(m):
         a,b = map(int,input().split())
-        graph[a][b] = 1
+        ladder[a][b] = 1
 
     answer = -1
 
