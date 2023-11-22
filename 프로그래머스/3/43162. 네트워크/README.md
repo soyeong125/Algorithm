1b1ad588d6966397eecb975f81d06f1d3654a73a
soyeong choi <57309311+soyeong125@users.noreply.github.com>
2023-11-22 11:54:17 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162/) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 0.55 ms
+메모리: 76.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 11:45:19
+2023년 11월 3일 11:54:15
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,51 +1,26 @@
 import java.util.*;
 
-class Solution {
-    
-    static int[] visited;
-    static Map<Integer, List<Integer>> map = new HashMap<>();
-    
-    void check(int start){
-        if(map.get(start) == null){
-            return;
-        }
-        for(Integer v : map.get(start)){
-            if(visited[v] == 0){
-                visited[v] = 1;
-                check(v);
-            }
-        }
-    }
-    
+class Solution {  
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] chk = new boolean[n];
         
         for(int i = 0 ; i < n ; i++){
-            for(int j = i+1 ; j < n ; j++){
-                    if(!map.containsKey(i)){
-                        map.put(i, new ArrayList<>());
-                    }
-                    if(!map.containsKey(j)){
-                        map.put(j, new ArrayList<>());
-                    }
-                    if(computers[i][j] == 1){
-                        map.get(i).add(j);
-                        map.get(j).add(i);
-                    }
-                }
-            }   
-        
-        visited = new int[n];
-        
-        for(int i = 0 ; i < n ; i++){
-            if(visited[i] == 1)
-                continue;
-            visited[i] = 1;
-            answer += 1;
-            check(i);
-            
+            if(!chk[i]){
+                dfs(i,computers,chk);
+                answer++;
+            }
         }
         
         return answer;
+    }
+    
+    void dfs (int start, int[][] computers, boolean[] chk){
+        chk[start] = true;
+        for(int i = 0 ; i < chk.length ; i++){
+            if(computers[start][i] == 1 && !chk[i]){
+                dfs(i,computers,chk);
+            }
+        }
     }
 }
